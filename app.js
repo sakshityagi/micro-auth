8826545a004f1dba9ef4e55196d96790409aa9a9
Sakshi Tyagi <tyagi.sakshi09@gmail.com>
2017-04-29 15:37:24 +0530

    Allowed cors

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const config = {
         password: "igdefault"
     }
 };
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
