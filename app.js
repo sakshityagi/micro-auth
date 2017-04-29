3ed4b0d0f286ee4685328b98f74e3b0193818fac
Sakshi Tyagi <tyagi.sakshi09@gmail.com>
2017-04-29 12:13:38 +0530

    Added bearer module in package.json

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+var user = require('./user');
+
+
+passport.use(new Strategy(
+  function(token, cb) {
+    db.users.findByToken(token, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
